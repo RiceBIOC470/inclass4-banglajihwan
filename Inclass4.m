7967f3686c7ed35aa74b719658fcc8be208c0932
banglajihwan <31257591+banglajihwan@users.noreply.github.com>
2017-09-09 16:01:15 -0500

    Add files via upload

--- Inclass4.m
+++ Inclass4.m
@@ -5,7 +5,41 @@
 %2" on its own line, then 9 lines with 7 random numbers each. (b) Write code
 %that reads only the first line of random numbers in the file and stores
 %them as numbers in an array. 
+rand_mat = rand(10,13);
+filename = 'rand_mat.txt';
+dlmwrite(filename, rand_mat, ' ') 
+filename1 = 'rn1.txt'
+fid=fopen(filename1,'w');
+fid1=fopen(filename,'r');
+print1 = 'Random numbers 1\n';
+fprintf(fid, print1);
+for line = 1:size(rand_mat,1)
+fprintf(fid, '%s\n',(fgetl(fid1)));
+end 
+rand_mat2 = rand(9,7);
+filename2 = 'rand_mat2.txt';
+dlmwrite(filename2, rand_mat2, ' ');
+fid2=fopen(filename2, 'r');
+print2 = 'Random numbers 2\n';
+fprintf(fid, print2);
+for line = 1:size(rand_mat2,1)
+fprintf(fid, '%s\n',(fgetl(fid2)));
+end 
+fclose('all')
+fid3 = fopen(filename1, 'r'); 
+for line = 1:2;
+    line2=fgetl(fid3); 
+end 
+line2 
+array1=str2num(line2)
 
 % 2. Write a function that takes an array as input and returns a logical
 % variable which is true if the sum of the numbers in the array is greater
 % than or equal to 10 and false if it is less than 10. 
+
+function result = greaterthan10 (array)
+array_sum=sum(array);
+if array_sum>=10
+    a = true 
+else a = false
+end 
